033b0bfaa82ba53b674495c1aaa8443ab981d585
Korkishko Sergey <Spiritvoin88@gmail.com>
2012-01-24 21:32:13 +0200

    #65 оплата и доставка

--- app/wp-content/themes/taobao2/page-dostavka-i-oplata.php
+++ app/wp-content/themes/taobao2/page-dostavka-i-oplata.php
@@ -1,177 +1,62 @@
 <?php get_header(); ?>
 
 <section id="container">
-			<section id="content">
-				<h2 class="title">Оптовые закупки с Taobao.ru.com</h2>
-				<div class="top"></div>
-				<div class="body">
-					<div class="allbox padding">
-						<ul class="bottons">
-							<li><a href="#">Оплата</a></li>
-							<li><a href="#">Доставка</a></li>
-							<li class="activ"><a href="#" class="big">Примерный расчет</a></li>
-							<li><a href="#">Калькулятор</a></li>
-						</ul>
-						<div class="slaider-box-page">
-							<span class="prev"></span>
-							<span class="next"></span>
-							<div class="slaide-page">
-								<ul>
-									<li>
-										<a href="#">ВНИМАНИЕ! Почта zakaz.taobao.ru.com@gmail.com временно не работает. Отправляйте письмо на почту zakaz@taobao.ru.com</a>
-									</li>
-									<li>
-										<a href="#">ВНИМАНИЕ! Почта zakaz.taobao.ru.com@gmail.com временно не работает. Отправляйте письмо на почту zakaz@taobao.ru.com</a>
-									</li>
-									<li>
-										<a href="#">ВНИМАНИЕ! Почта zakaz.taobao.ru.com@gmail.com временно не работает. Отправляйте письмо на почту zakaz@taobao.ru.com</a>
-									</li>
-								</ul>
-							</div>					
-						</div>
-						<div class="boxer-text">
-							<h2>Оплата</h2>
-							<p>Оплатить заказ можно с помощью перевода на счет карты «Сбербанка» (<a href="#">вы  можете это сделать не выходя из дома</a>), «ВТБ24″, блиц-перевода «Сбербанка», «Золотая Корона» или перевода «Western Union».</p>
-							<p><b>Происходит оплата в 3 этапа:</b></p>
-							<ul>
-								<li>
-									<span class="number">1</span>
-									<div class="txt">
-										<p>Оплата выбранных вещей и доставки по Китаю с учетом нашей комиссии (15 % от стоимости товара на сайте <a href="#">www.taobao.com</a>)</p>
-										<p>Комиссия не берется за доставку товара по Китаю.</p>
-									</div>
-								</li>
-								<li>
-									<span class="number">2</span>
-									<div class="txt">
-										<p>Оплата за доставку из Китая в Россию (г.Благовещенск) по тарифу от <b>300 руб/1 кг.</b></p>
-										<p>НОВИНКА! «ФОТООТЧЕТ» доступен каждому:</p>
-										<dl>
-											<dt><b>1-25 позиций</b> = 100 руб;</dt>
-											<dt><b>25-50 позиций</b> = 200 руб;</dt>
-											<dt><b>более 50 позиций</b> = 250-500 руб.</dt>
-										</dl>
-									</div>
-								</li>
-								<li>
-									<span class="number">3</span>
-									<div class="txt">
-										<p>Оплата за перевозку по России производится согласно тарифам транспортных компаний</p>
-										<p><i>Отправляем груз всеми транспортными компаниями, кроме «Почты России».</i></p>
-									</div>	
-								</li>
-							</ul>
-						</div>
-					</div>					
-					<div class="alamo">
-						<h2>Расчет</h2>
-						<div class="calculator">
-							<span>15<i>%</i></span>
-							<span>5,3</span>
-						</div>
-						<ul class="inform">
-							<li>- доставка по Китаю выбирается EMS;</li>
-							<li><span class="num">15%</span> -  наша комиссия, которая может изменяться на 5-10 % (для оптовиков и посредников);</li>
-							<li><span class="num">5,3</span> - внутренний курс юаня нашей компании;</li>
-							<li>- 300 руб /кг – тарифы компании карго из Хэйхэ (Китай) до Благовещенска (Россия).</li>
-						</ul>
-					</div>
-					<div class="block">
-						<h2>Доставка</h2>
-						<table cellpadding="0" class="icons" cellspacing="0">
-							<tr>
-								<td>
-									<a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/ico.gif" alt="" title="" /></a>
-									<span><b>Авиа доставка</b> по территории РФ</span>
-								</td>
-								<td>
-									<a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/ico1.gif" alt="" title="" /></a>
-									<span><b>Ж/д доставка</b> по территории РФ</span>
-								</td>
-								<td>
-									<a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/ico2.gif" alt="" title="" /></a>
-									<span><b>Автодоставка</b> по Дальнему востоку и Амурской области.</span>
-								</td>
-							</tr>
-						</table>
-						<div class="boxer-text">
-							<p>Время доставки начинает исчисляться с того момента, как Вы подтвердили заказ, оплатили его и деньги поступили на наш счет:</p>
-							<p><b>- по Китаю 10-18 дней;</b></p>
-							<p><b>- из Китая в Благовещенск</b> отправляем карго 2 раза в неделю, в среднем доставка занимает 3-5 дней с момента получения груза на складе в Китае;</p>
-							<p>- если заказчик находится в другом городе России, срок доставки до Вашего города определяется транспортной компанией. Оплата за перевозку по России может быть как в Благовещенске, так и на месте получения!</p>
-							<p><i>Отправляем всеми транспортными компаниями, кроме «Почты России».</i></p>
-						</div>
+    <section id="content">
+        <h2 class="title">Оптовые закупки с Taobao.ru.com</h2>
 
-                        <?php require_once "functions/transport-companies.php"; ?>
+        <div class="top"></div>
+        <div class="body">
+            <div class="allbox padding">
+                <ul class="bottons">
+                    <li><a href="#">Оплата</a></li>
+                    <li><a href="#">Доставка</a></li>
+                    <li class="activ"><a href="#" class="big">Примерный расчет</a></li>
+                    <li><a href="#">Калькулятор</a></li>
+                </ul>
+                <div class="slaider-box-page">
+                    <span class="prev"></span>
+                    <span class="next"></span>
 
-					</div>
-					<div class="minus">
-						<form action="" method="post" class="color">
-							<div class="text-form">
-								<h2>Калькулятор стоимости доставки</h2>
-								<p>Для удобства расчета стоимости товара с учетом доставки, воспользуйтесь приведенной ниже формой. </p>
-							</div>							
-							<div class="item">
-								<label>Стоимость товара на таобао (юани)</label>
-								<input type="text" class="text" />
-								<a href="#" class="info"></a>
-							</div>
-							<div class="item">
-								<label>Стоимость доставки по Китаю (юани)</label>
-								<input type="text" class="text" />
-								<a href="#" class="info"></a>
-							</div>
-							<div class="item">
-								<label>Вес посылки (кг.)</label>
-								<input type="text" class="text" />
-								<a href="#" class="info"></a>
-							</div>
-							<div class="item">
-								<label>Ваше местоположение</label>
-								<select class="sel">
-									<option>не выбрано</option>
-									<option>1</option>
-									<option>1</option>
-									<option>1</option>
-								</select>
-							</div>													
-							<div class="item">						
-								<label>Оптовый заказ <input clsss="chek" type="checkbox" /></label>
-							</div>	
-							<div class="item">	
-								<button type="submit" class="sub"><i>Рассчитать</i></button>
-							</div>
-						</form>
-					</div>
-					<div class="thanks">
-						<h2>Наш режим работы</h2>
-						<table cellpadding="0" cellspacing="0">
-							<tr>
-								<td>Понедельник - пятница</td>
-								<td><span>10:00 – 19:00</span></td>
-							</tr>
-							<tr>
-								<td>Суббота - воскресенье</td>
-								<td><span>выходной</span></td>
-							</tr>
-						</table>
-						<div class="center">
-							<p>Если у вас появились вопросы – <a href="#">задайте их менеджеру!</a>  Работаем мы –  улыбаетесь вы! :)</p>
-							<h3>Удачных покупок и быстрой доставки!</h3>
-						</div>
-					</div>
-				</div>
-				<div class="bottom"></div>
-			</section>
-            <div class="right">
-                <div class="boxen">
-                    <?php get_sidebar('calc') ?>
+                    <div class="slaide-page">
+                        <ul>
+                            <li>
+                                <a href="#">ВНИМАНИЕ! Почта zakaz.taobao.ru.com@gmail.com временно не работает.
+                                    Отправляйте письмо на почту zakaz@taobao.ru.com</a>
+                            </li>
+                            <li>
+                                <a href="#">ВНИМАНИЕ! Почта zakaz.taobao.ru.com@gmail.com временно не работает.
+                                    Отправляйте письмо на почту zakaz@taobao.ru.com</a>
+                            </li>
+                            <li>
+                                <a href="#">ВНИМАНИЕ! Почта zakaz.taobao.ru.com@gmail.com временно не работает.
+                                    Отправляйте письмо на почту zakaz@taobao.ru.com</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="blog-gree">
-                    <?php get_sidebar('blog') ?>
-                </div>
+                <?php wp_reset_query(); ?>
+                <?php rewind_posts(); ?>
+                <?php  while (have_posts()) : the_post(); ?>
+                <?php the_content(); ?>
+                <?php endwhile; ?>
             </div>
-		</section>
-	</div>
+            <?php query_posts('post_parent=dostavka-i-oplata&post_type=page&numberposts=1');?>
+            <?php while (have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
+            <?php endwhile;?>
+            <?php wp_reset_query();?>
+        </div>
+        <div class="bottom"></div>
+    </section>
+    <div class="right">
+        <div class="boxen">
+            <?php get_sidebar('calc') ?>
+        </div>
+        <div class="blog-gree">
+            <?php get_sidebar('blog') ?>
+        </div>
+    </div>
+</section>
+</div>
 
 <?php get_footer(); ?>
