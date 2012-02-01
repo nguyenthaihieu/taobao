8504e1fea94a5fdc8ec07cd6aafb1f24c58bba09
Aleksey Razbakov <aleksey@razbakov.com>
2012-02-01 16:38:03 +0200

    geoip by country

--- app/wp-content/themes/taobao2/js/geoip.js
+++ app/wp-content/themes/taobao2/js/geoip.js
@@ -1,16 +1,9 @@
-if(clientIP) {
-    $.get("http://ipgeobase.ru:7020/geo?ip="+clientIP.toString(), function(data) {
-        var mycity = $(data).find("city").text();
-        if(mycity) {
-            $("#cusel-scroll-search_country").append('<span val="my">'+mycity+'</span>');
-            var params = {
-                refreshEl: "#search_country",
-                visRows: 16,
-                scrollArrows: true
-            }
-            cuSelRefresh(params);
-
-        }
-        return true;
-    }, "xml")
-}
+$(function(){
+  if(clientIP) {
+      $.get("http://api.hostip.info/country.php?ip="+clientIP.toString(), function(data) {
+          if(data) {
+              $("#search_country").val(data);
+          }
+      })
+  }
+})
