a862f67c55971790c6a78d1e02ffc15bbb0416b8
Aleksey Razbakov <aleksey@razbakov.com>
2012-02-01 16:45:49 +0200

    added cookie for country

--- app/wp-content/themes/taobao2/js/geoip.js
+++ app/wp-content/themes/taobao2/js/geoip.js
@@ -3,6 +3,8 @@ $(function(){
       $.get("http://api.hostip.info/country.php?ip="+clientIP.toString(), function(data) {
           if(data) {
               $("#search_country").val(data);
+              if(!$.cookie('country'))
+                $.cookie('country', currentCountry);
           }
       })
   }
