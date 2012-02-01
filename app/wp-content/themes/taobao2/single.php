068172ca3aa7d6380c8a3fe978a417cc18ba3023
Aleksey Razbakov <aleksey@razbakov.com>
2012-02-01 11:58:25 +0200

    Removed picture from comments

--- app/wp-content/themes/taobao2/single.php
+++ app/wp-content/themes/taobao2/single.php
@@ -26,9 +26,9 @@ function catch_that_image() {
 					<div class="page">
 						<h2><?php the_title(); ?></h2>
 							
-									<p><?php the_content(); ?></p>
+                        <p><?php the_content(); ?></p>
 							
-												<div class="article">
+                        <div class="article">
 							<span class="label"><?php the_tags('', ', ', '<br />'); ?></span>
 							<?php endwhile; else: ?>
 							<p><?php _e('По вашему запросу ничего нет.'); ?></p>
@@ -36,39 +36,10 @@ function catch_that_image() {
 							<a class="com"><?php comments_number(0,$onenumber,$morenumber);?></a>
 							<span class="data"><?php the_time('d.m.Y');?></span> 
 						</div>
-						<div class="soc"><img src="img/soc.gif" alt="" title="" /></div>	
 						<div class="comentars">
 							<h3>Комментарии</h3>
 							<?php get_template_part( 'content', 'single' ); ?>
 							<?php comments_template( '', true ); ?>
-							<!--<form action="" method="post">
-								<div class="item">
-									<div class="ava"><a href="#"><img src="img/ava.gif" alt="" title="" /></a></div>
-									<textarea></textarea>
-								</div>
-								<div class="coment">
-									<div class="ava"><a href="#"><img src="img/ava.gif" alt="" title="" /></a></div>
-									<div class="boxer">
-										<p><a href="#" class="name">T-ula</a> <span class="date">22 ноября 2011, 20:55</span></p>
-										<p>Здравствуйте! Хотела бы заказать 5 брендовых курточек (одной модели), не будет ли проблем с таможней? Обычно многие поставщики не советуют заказывать более 4-х шт.  одной модели?</p>
-									</div>
-									<div class="podcoment">
-										<div class="ava"><a href="#"><img src="img/ava.gif" alt="" title="" /></a></div>
-										<div class="boxe">
-											<p><a href="#" class="nameblue">T-ula</a> <span class="date">22 ноября 2011, 20:55</span></p>
-											<p>Здравствуйте! Хотела бы заказать 5 брендовых курточек (одной модели), не будет ли проблем с таможней? Обычно многие поставщики не советуют заказывать более 4-х шт.  одной модели?</p>
-										</div>
-									</div>
-								</div>
-								<div class="coment">
-									<div class="ava"><a href="#"><img src="img/ava.gif" alt="" title="" /></a></div>
-									<div class="boxer">
-										<p><a href="#" class="name">T-ula</a> <span class="date">22 ноября 2011, 20:55</span></p>
-										<p>Здравствуйте! Хотела бы заказать 5 брендовых курточек (одной модели), не будет ли проблем с таможней? Обычно многие поставщики не советуют заказывать более 4-х шт.  одной модели?</p>
-									</div>
-								</div>
-							</form>-->
-
 							<ul class="rss">
 								<li><a href="<?php bloginfo('rss2_url'); ?>" class="rss">RSS</a></li>
 								<li><a href="#" class="mail"></a></li>
