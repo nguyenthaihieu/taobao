48822775992dccf8e433cfd056aa49fd10cec03b
sergey martishko <sergey.martishko@yandex.ua>
2012-02-20 15:49:42 +0300

    delite

--- app/wp-content/themes/taobao/js/geoip.js
+++
@@ -1,6 +0,0 @@
-$.get("http://ipgeobase.ru:7020/geo?ip=<?=$_SERVER['REMOTE_ADDR']?>", function(data) {
-    var mycity = $(data).find("city").text();
-    $("#city").text(mycity);
-    return true;
-}, "xml")
-
